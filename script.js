9c02ab82624c77301844fb2c739a6f2e5917869e
Jujej <jpdurau@gmail.com>
2023-11-16 15:26:39 -0300

    final

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let cliques = 0;
+    const iniciarChatBtn = document.getElementById('iniciarChat');
+    const campoIdade = document.getElementById('campoIdade');
+    const botaoCaptcha = document.getElementById('botaoCaptcha');
+    const chatButton = document.getElementById('chatButton');
+    const chatBox = document.getElementById('chatBox');
+    const enviarMensagemBtn = document.getElementById('enviarMensagem');
+    const mensagemInput = document.getElementById('mensagemInput');
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const mobileMenu = document.querySelector(".mobile-menu");
-    const nav = document.querySelector(".nav-header");
+    iniciarChatBtn.addEventListener('click', function () {
+        campoIdade.style.display = 'block';
+    });
 
-    mobileMenu.addEventListener('click', () => {
-        console.log("Menu móvel clicado"); // Adicione esta linha para debugar
-        nav.classList.toggle("active");
+    botaoCaptcha.addEventListener('click', function () {
+        cliques++;
+
+        if (cliques === 5) {
+            console.log('Captcha Fictício validado. Exibindo botão de chat...');
+            botaoCaptcha.style.display = 'none';
+             chatBox.classList.remove('hidden');
+        } else {
+            console.log(`Clique ${cliques} de 5 no Captcha Fictício.`);
+        }
     });
 });
